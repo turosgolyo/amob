239179bad89cd823914bdeec58cdf90bf6c6cd4b
turosgolyo <papp.dani2206@gmail.com>
2025-09-10 10:16:02 +0200

    2025-09-10

--- dart/vers.dart
+++ dart/vers.dart
@@ -2,8 +2,16 @@ import 'dart:io';
 
 void main(){
     print("Irjon be egy verset:");
-    String? falvedo = stdin.readLineSync();
+    String falvedo = "Mindenki Igaza Sajátja    SZUBJEKTÍV   IGAZSÁG   Objektív Nincs is az   ";
 
-    print("Eredeti: ${falvedo}");
-
+    print("${falvedo}");
+    print("${falvedo.toLowerCase()}");
+    print("${falvedo.toUpperCase()}");
+    print("${falvedo.trim()}");
+    print("${falvedo.replaceAll(" ", "-")}");
+    print("${falvedo.substring(4)}");
+    print("${falvedo.codeUnitAt(0)}");
+    print("${falvedo.codeUnitAt(1)}");
+    print("${falvedo.codeUnitAt(2)}");
+    print("${falvedo.substring(9)}");
 }
